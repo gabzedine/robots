615250a3185d07cc4abfcf1dea231e24d579596b
gabzedine <gblosr@gmail.com>
2019-12-30 16:31:00 -0300

    concluido: criação de thumbnail para vídeo

--- robots/image.js
+++ robots/image.js
@@ -1,5 +1,7 @@
 const imageDownloader = require("image-downloader");
-const gm = require("gm").subClass({ imageMagick: true });
+const gm = require("gm").subClass({
+    imageMagick: true
+});
 const google = require("googleapis").google;
 const customSearch = google.customsearch("v1");
 const state = require("./state.js");
@@ -7,128 +9,217 @@ const state = require("./state.js");
 const googleSearchCredentials = require("../credentials/google-search.json");
 
 async function robot() {
-  const content = state.load();
+    const content = state.load();
 
-  // await fetchImageOfAllSentences(content)
-  //   await downloadAllImages(content);
-  await convertAllImages(content);
+    // await fetchImageOfAllSentences(content)
+    // await downloadAllImages(content);
+    // await convertAllImages(content);
+    // await createAllSentenceImage(content);
+    await createYouTubeThumbnail()
 
-  // state.save(content)
+    // state.save(content)
 
-  async function fetchImageOfAllSentences(content) {
-    for (const sentence of content.sentences) {
-      const query = `${content.searchTerm} ${sentence.keywords[0]}`;
-      sentence.images = await fetchGoogleAndReturnImagesLinks(query);
+    async function fetchImageOfAllSentences(content) {
+        let thisKeyword = ''
+        let arrSearchTerm = content.searchTerm.split(" ")
+        for (const sentence of content.sentences) {
+            if (sentence.keywords[0].indexOf(arrSearchTerm[0])) {
+                thisKeyword = sentence.keywords[0]
+            } else if (sentence.keywords[1].indexOf(arrSearchTerm[0])) {
+                thisKeyword = sentence.keywords[1]
+            } else if (sentence.keywords[2].indexOf(arrSearchTerm[0])) {
+                thisKeyword = sentence.keywords[2]
+            } else {
+                thisKeyword = sentence.keywords[3]
+            }
 
-      sentence.googleSeachQuery = query;
-    }
-  }
+            const query = `${content.searchTerm} ${thisKeyword}`;
+            sentence.images = await fetchGoogleAndReturnImagesLinks(query);
 
-  async function fetchGoogleAndReturnImagesLinks(query) {
-    const response = await customSearch.cse.list({
-      auth: googleSearchCredentials.apiKey,
-      cx: googleSearchCredentials.searchEngineId,
-      q: query,
-      searchType: "image",
-      // imgSize: "huge",
-      num: 2
-    });
-
-    const imagesUrl = response.data.items.map(item => {
-      return item.link;
-    });
-
-    return imagesUrl;
-  }
-
-  async function downloadAllImages(content) {
-    content.downloadedImages = [];
-
-    for (
-      let sentenceIndex = 0;
-      sentenceIndex < content.sentences.length;
-      sentenceIndex++
-    ) {
-      const images = content.sentences[sentenceIndex].images;
-
-      for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
-        const imageUrl = images[imageIndex];
-
-        try {
-          if (content.downloadedImages.includes(imageUrl)) {
-            throw new Error(`> [image-robot] Imagem ja foi baixada\n`);
-          }
-
-          await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`);
-          content.downloadedImages.push(imageUrl);
-          console.log(
-            `> [image-robot] [${sentenceIndex}] [${imageIndex}] Baixou imagem com sucesso: ${imageUrl}\n`
-          );
-          break;
-        } catch (error) {
-          console.log(
-            `> [image-robot] [${sentenceIndex}] [${imageIndex}] Erro ao baixar ${imageUrl}: ${error}\n`
-          );
+            sentence.googleSeachQuery = query;
         }
-      }
     }
-  }
 
-  async function downloadAndSave(url, fileName) {
-    return imageDownloader.image({
-      url,
-      url,
-      dest: `./content/${fileName}`
-    });
-  }
-
-  async function convertAllImages(content) {
-    for (
-      let sentenceIndex = 0;
-      sentenceIndex < content.sentences.length;
-      sentenceIndex++
-    ) {
-      await convertImage(sentenceIndex);
-    }
-  }
-
-  async function convertImage(sentenceIndex) {
-    return new Promise((resolve, reject) => {
-      const inputFile = `./content/${sentenceIndex}-original.png[0]`;
-      const outputFile = `./content/${sentenceIndex}-converted.png`;
-      const width = 1920;
-      const height = 1080;
-
-      gm()
-        .in(inputFile)
-        .out("(")
-        .out("-clone")
-        .out("0")
-        .out("-background", "white")
-        .out("-blur", "0x9")
-        .out("-resize", `${width}x${height}^`)
-        .out(")")
-        .out("(")
-        .out("-clone")
-        .out("0")
-        .out("-background", "white")
-        .out("-resize", `${width}x${height}`)
-        .out(")")
-        .out("-delete", "0")
-        .out("-gravity", "center")
-        .out("-compose", "over")
-        .out("-composite")
-        .out("-extent", `${width}x${height}`)
-        .write(outputFile, error => {
-          if (error) {
-            return reject(error);
-          }
-
-          console.log(`> [image-robot] Image converted: ${outputFile}`);
-          resolve();
+    async function fetchGoogleAndReturnImagesLinks(query) {
+        const response = await customSearch.cse.list({
+            auth: googleSearchCredentials.apiKey,
+            cx: googleSearchCredentials.searchEngineId,
+            q: query,
+            searchType: "image",
+            // imgSize: "huge",
+            num: 2
         });
-    });
-  }
+
+        const imagesUrl = response.data.items.map(item => {
+            return item.link;
+        });
+
+        return imagesUrl;
+    }
+
+    async function downloadAllImages(content) {
+        content.downloadedImages = [];
+
+        for (
+            let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++
+        ) {
+            const images = content.sentences[sentenceIndex].images;
+
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+                const imageUrl = images[imageIndex];
+
+                try {
+                    if (content.downloadedImages.includes(imageUrl)) {
+                        throw new Error(`> [image-robot] Imagem ja foi baixada\n`);
+                    }
+
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`);
+                    content.downloadedImages.push(imageUrl);
+                    console.log(
+                        `> [image-robot] [${sentenceIndex}] [${imageIndex}] Baixou imagem com sucesso: ${imageUrl}\n`
+                    );
+                    break;
+                } catch (error) {
+                    console.log(
+                        `> [image-robot] [${sentenceIndex}] [${imageIndex}] Erro ao baixar ${imageUrl}: ${error}\n`
+                    );
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, fileName) {
+        return imageDownloader.image({
+            url,
+            url,
+            dest: `./content/${fileName}`
+        });
+    }
+
+    async function convertAllImages(content) {
+        for (
+            let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++
+        ) {
+            await convertImage(sentenceIndex);
+        }
+    }
+
+    async function convertImage(sentenceIndex) {
+        return new Promise((resolve, reject) => {
+            const inputFile = `./content/${sentenceIndex}-original.png[0]`;
+            const outputFile = `./content/${sentenceIndex}-converted.png`;
+            const width = 1920;
+            const height = 1080;
+
+            gm()
+                .in(inputFile)
+                .out("(")
+                .out("-clone")
+                .out("0")
+                .out("-background", "white")
+                .out("-blur", "0x9")
+                .out("-resize", `${width}x${height}^`)
+                .out(")")
+                .out("(")
+                .out("-clone")
+                .out("0")
+                .out("-background", "white")
+                .out("-resize", `${width}x${height}`)
+                .out(")")
+                .out("-delete", "0")
+                .out("-gravity", "center")
+                .out("-compose", "over")
+                .out("-composite")
+                .out("-extent", `${width}x${height}`)
+                .write(outputFile, error => {
+                    if (error) {
+                        return reject(error);
+                    }
+
+                    console.log(`> [image-robot] Image converted: ${outputFile}`);
+                    resolve();
+                });
+        });
+    }
+
+    async function createAllSentenceImage(content) {
+        for (
+            let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++
+        ) {
+            await createSentenceImage(
+                sentenceIndex,
+                content.sentences[sentenceIndex].text
+            );
+        }
+    }
+
+    async function createSentenceImage(sentenceIndex, sentenceText) {
+        return new Promise((resolve, reject) => {
+            const outputFile = `./content/${sentenceIndex}-sentence.png`;
+
+            const templateSettings = {
+                0: {
+                    size: "1920x400",
+                    gravity: "center"
+                },
+                1: {
+                    size: "1920x1080",
+                    gravity: "center"
+                },
+                2: {
+                    size: "800x1080",
+                    gravity: "west"
+                },
+                3: {
+                    size: "1920x400",
+                    gravity: "center"
+                },
+                4: {
+                    size: "1920x1080",
+                    gravity: "center"
+                },
+                5: {
+                    size: "800x1080",
+                    gravity: "west"
+                },
+                6: {
+                    size: "1920x400",
+                    gravity: "center"
+                }
+            };
+
+            gm()
+                .out("-size", templateSettings[sentenceIndex].size)
+                .out("-gravity", templateSettings[sentenceIndex].gravity)
+                .out("-background", "transparent")
+                .out("-fill", "white")
+                .out("-kerning", "-1")
+                .out(`caption:${sentenceText}`)
+                .write(outputFile, error => {
+                    if (error) {
+                        return reject(error);
+                    }
+
+                    console.log(`> [video-robot] Sentence created: ${outputFile}`);
+                    resolve();
+                });
+        });
+    }
+
+    async function createYouTubeThumbnail() {
+        return new Promise((resolve, reject) => {
+            gm()
+                .in('./content/0-converted.png')
+                .write('./content/youtube-thumbnail.jpg', (error) => {
+                    if (error) {
+                        return reject(error)
+                    }
+
+                    console.log('> [image-robot] YouTube thumbnail created')
+                    resolve()
+                })
+        })
+    }
 }
 
 module.exports = robot;
